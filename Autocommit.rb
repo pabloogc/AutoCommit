d0268f47bad5552fc035f6a23d3202fa8e890b54
Pablo Orgaz <pabloogc@gmail.com>
2016-11-07 17:23:08 +0100

    Derpy hooves

--- Autocommit.rb
+++ Autocommit.rb
@@ -2,38 +2,18 @@
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
-class Autocommit < Formula
+class AutoCommit < Formula
   desc "http://whathecommit.com tool written in Nim"
   homepage "https://github.com/pabloogc/AutoCommit"
   url "https://github.com/pabloogc/AutoCommit/releases/download/0.1/autocommit-0.1.tar.gz"
   version "0.1"
   sha256 "db78eade67a415a4e25912b182136629986718eed88923bff971719778ddc556"
 
-  # depends_on "cmake" => :build
-  # depends_on :x11 # if your formula requires any X11/XQuartz components
-
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    bin.install "autocommit"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test autocommit`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "true"
   end
 end
