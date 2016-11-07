fd3bdad713999ed31161f101c05e66c2f6df7a95
Pablo Orgaz <pabloogc@gmail.com>
2016-11-07 17:07:34 +0100

    Update formula.rb

--- formula.rb
+++ formula.rb
@@ -4,7 +4,7 @@
 
 class Autocommit < Formula
   desc "http://whathecommit.com tool written in Nim"
-  homepage ""
+  homepage "https://github.com/pabloogc/AutoCommit"
   url "https://github.com/pabloogc/AutoCommit/releases/download/0.1/autocommit-0.1.tar.gz"
   version "0.1"
   sha256 "db78eade67a415a4e25912b182136629986718eed88923bff971719778ddc556"
